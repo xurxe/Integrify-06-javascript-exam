6d276b3cba0fd2db3821c5822d33bf2b033bec54
Xurxe <xurxe.garcia@integrify.io>
2019-03-26 17:47:35 +0200

    Made changes, added license.md

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function countOccurences(paragraph, word) {
     let count = (matches || []).length;
 
     return count;
-}
+};
 
 const paragraph = 'I love teaching. If you do not love teaching what else can you love. I love JavaScript if you do not love something which can give life to your application what else can you love.';
 console.log(countOccurences(paragraph, 'love'));
